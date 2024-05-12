b3db996b9407942dbb53874e628d409682b0fe74
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-05-12 21:48:08 +0800

    Update FirstClass.php

--- namespace-autoloader/App/FirstClass.php
+++ namespace-autoloader/App/FirstClass.php
@@ -4,8 +4,10 @@ declare(strict_types= 1);
 namespace App;
 
 class FirstClass {
+    
     public function __construct(
         public string $name,
         public string $email,
     ) {}
+    
 }
