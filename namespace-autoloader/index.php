cd924dc5c9d7b4cdbb048f9bb41ac349f091b141
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-05-12 21:25:16 +0800

    Create index.php

---
+++ namespace-autoloader/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * This task is to complete the Live coding session with the Lead Developer from the company that I applied for Senior PHP Developer position.
+ * 
+ * I've asked his permission to publish this code.
+ * 
+ * There are many tasks that I need to code during the interview. However, I only show this part only for others reference
+ */
