6989f9363befae655796110731c65b6ba3aaabfa
Muhamad Sulaiman Misri <47172118+msulaimanmisri@users.noreply.github.com>
2024-05-12 21:47:04 +0800

    Update index.php

--- namespace-autoloader/index.php
+++ namespace-autoloader/index.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types= 1);
 
 /**
  * This task is to complete the Live coding session with the Lead Developer from the company that I applied for Senior PHP Developer position.
@@ -7,3 +8,9 @@
  * 
  * There are many tasks that I need to code during the interview. However, I only show this part only for others reference
  */
+
+ use App\FirstClass;
+ use App\SecondClass;
+
+ $firstClassData = new FirstClass('sulaiman', 'hello@sulaimanmisri.com');
+ $secondClassData = new SecondClass('12_500', 'cimb', '97745-54475-5224');
